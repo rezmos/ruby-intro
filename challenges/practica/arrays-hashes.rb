c0e7e5598e07df5a68e8df4c7ba2bf5baba93faf
juan267 <j.gomez267@gmail.com>
2017-02-15 18:36:09 -0500

    nested structures practice

--- challenges/practica/arrays-hashes.rb
+++ challenges/practica/arrays-hashes.rb
@@ -10,6 +10,8 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 # 2. Para mantenernos organizados, organiza el array zombie_apocalypse_supplies de forma alfabetica.
 # ----
 
+ result == ["binoculars", "CB radio", "compass", "hatchet", "rations","shotgun", "Tactical 10mm", "water jug"]
+
 # 3. Crea un metodo que mire si un item (string) esta dentro del arreglo zombie_apocalypse_supplies. Por ejemplo esta `boots` dentro de la lista de suministros?.
 # ----
 
@@ -52,3 +54,51 @@ extinct_animals = {
 # 5. Acabamos de encontrar que el Passenger Pigeon, realmente no esta extincto!
 # Remuevelo del hash extinct_animals y devuelve su pareja de key/value como un arreglo de dos elementos. Puedes encontrar un metodo en la documentacion de la  clase Hash que te puede ayudar con esto.
 # ----
+
+
+# Practica, Estructuras nesteadas
+
+# Imprime en la consola solo el elemento target ningun otro
+
+# Target: "FORE"
+
+array = [[1,2], ["inner", ["eagle", "par", ["FORE", "hook"]]]]
+
+# Intentos:
+# ============================================================
+
+
+
+# ============================================================
+
+# Target: "congrats!"
+
+hash = {outer: {inner: {"almost" => {3 => "congrats!"}}}}
+
+# Intentos:
+# ============================================================
+
+
+
+# ============================================================
+
+# Target: "finished"
+
+nested_data = {array: ["array", {hash: "finished"}]}
+
+# Intentos:
+# ============================================================
+
+
+
+# ============================================================
+
+#Usando un metodo del modulo Enumerable, modifica el array number_array de forma que cada numero se le sume 5. Lo vas a hacer de forma destructiva o no ?
+
+number_array = [5, [10, 15], [20,25,30], 35]
+
+
+
+# Crea un metodo que recina un array como argumento, en este caso el array startup_names, y devuelva un array igual pero en donde a cada nombre se le a añadido 'ly' al final.
+
+startup_names = ["bit", ["find", "fast", ["optimize", "scope"]]]
