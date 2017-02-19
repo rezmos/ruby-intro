8136fd7db1bd1540766c88b22bea09da460adea7
rezmos <rezmos@gmail.com>
2017-02-19 09:05:37 -0500

    Exercise 3 - using artihmetic operators

--- challenges/3-local-variables/my_solution.rb
+++ challenges/3-local-variables/my_solution.rb
@@ -3,14 +3,13 @@
 
 # Tu solucion abajo:
 
-num1 = 4
-num2 = 9
-
-sum = num2 + num1
+num1 = 2;
+num2 = 3;
+sum = num1 + num2
 difference = num1 - num2
 quotient = num1.to_f / num2
-modulus = num1 % num2
-product = num1 * num2
+product = num1* num2
+modulus = (num1.to_f%num2)
 
 # Pruebas NO LAS BORRES
 
