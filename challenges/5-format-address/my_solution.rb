9ce93fd3e48f6f9ebbfdbabdcbdcf60480740d30
rezmos <rezmos@gmail.com>
2017-02-26 08:32:55 -0500

    [CA] Using concat operators

--- challenges/5-format-address/my_solution.rb
+++ challenges/5-format-address/my_solution.rb
@@ -1,7 +1,6 @@
 # Format an Address
 
 # Tu solucion abajo:
-
-def make_address(street,city,state,zip)
-  "You live at #{street}, in the beautiful city of #{city}, #{state}. Your zip is #{zip}."
+def make_address(street, city, state, zip)
+  return "You live at #{street}, in the beautiful city of #{city}, #{state}. Your zip is #{zip}."
 end
