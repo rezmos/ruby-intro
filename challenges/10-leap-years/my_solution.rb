a48443a8fd044d909526cbce35d241c5a66b4df0
rezmos <rezmos@gmail.com>
2017-03-05 08:59:03 -0500

    [CA] if conditionals (leap year)

--- challenges/10-leap-years/my_solution.rb
+++ challenges/10-leap-years/my_solution.rb
@@ -1,54 +1,7 @@
-# Leap Years
-
-# # Tu solucion abajo:
-# condition = true
-
-# unless !condition
-#   p "algo"
-# end
-
-# p 'blahah' if condition
-
-
-# if condition
-#   p 'Blahah'
-# end
-
-
-# if condition
-#   p 'if'
-# else
-#   p 'else'
-# end
-
-# condition ?  p 'if' : p 'else'
-# def leap_year?(year)
-#   if year % 4 == 0 || year % 400 == 0
-#     return true unless year % 100 == 0 && year % 400 != 0
-#     false
-#   else
-#     false
-#   end
-# end
-
-def leap_year?(year)
-  if year % 4 == 0 && year / 100 != 0 || year % 400 == 0
-    true
+def leap_year?(año)
+  if((año%4==0 && año%100!=0)||año%400==0)
+    return true
   else
-    false
+    return false
   end
 end
-
-# def leap_year?(n)
-#   if n % 4 == 0 && n % 100 == 0 && n % 400 != 0
-#     false
-#   elsif n % 400 == 0
-#     true
-#   elsif n % 4 == 0 && n % 100 == 0
-#     true
-#   elsif n % 4 == 0
-#     true
-#   else
-#     false
-#   end
-# end
