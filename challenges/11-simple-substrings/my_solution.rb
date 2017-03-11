f8a581920e079b57edabbe19bca81e9a3dfa4068
rezmos <rezmos@gmail.com>
2017-03-11 10:24:12 -0500

    [CA] learning include? String method

--- challenges/11-simple-substrings/my_solution.rb
+++ challenges/11-simple-substrings/my_solution.rb
@@ -2,12 +2,10 @@
 
 # Tu solucion abajo:
 
-def welcome(str)
-  if str.include? "CA"
+def welcome(adress)
+  if(adress.include? "CA")
     return "Welcome to California"
   else
     return "You should move to California"
   end
 end
-
-puts welcome("CA")
