ac84d545a4570b9f8a362bd1a426a30a3d3f7ddd
rezmos <rezmos@gmail.com>
2017-03-11 11:55:47 -0500

    [CA] Using Arrays (first approach) - for each

--- challenges/12-smallest-integer/my_solution.rb
+++ challenges/12-smallest-integer/my_solution.rb
@@ -4,5 +4,15 @@
 
 # Tu solucion abajo:
 
-
-
+def smallest_integer(numbers)
+    smallest = numbers[0];
+    if numbers.empty?
+      return nil
+    end
+    numbers.each do |number|
+      if number < smallest
+         smallest = number
+      end
+    end
+    return smallest
+end
