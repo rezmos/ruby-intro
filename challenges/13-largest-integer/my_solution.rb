b5ec42f44552f2fddea106d03d4160009b19c0d5
rezmos <rezmos@gmail.com>
2017-03-11 12:06:06 -0500

    [CA] Using arrays and for each

--- challenges/13-largest-integer/my_solution.rb
+++ challenges/13-largest-integer/my_solution.rb
@@ -1,2 +1,16 @@
 # Largest Integer
+def largest_integer (numbers)
+  if (numbers.empty?)
+    return nil
+  end
 
+  largest = numbers[0]
+
+  numbers.each  do |number|
+    if(number>largest)
+      largest = number
+    end
+  end
+
+  return largest
+end
